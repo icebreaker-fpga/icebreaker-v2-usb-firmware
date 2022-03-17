3547a9ce9d4a27a968e8d20dbae5474d4f23a81c
Greg Davill <greg.davill@gmail.com>
2022-03-17 21:32:16 +1030

    Add basic meson based project

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # icebreaker-v2-usb-firmware
+
 Firmware to implement USB communications on the CH32V307 microcontroller
+
+## Prerequisites
+
+- RISCV compiler (xPack)
+- Meson
+
+## Building
+
+```text
+meson setup build/ --cross-file riscv-xpack
+meson compile -C build/
+```
+
+## License
+
+Unless otherwise stated files are licensed as Apache-2.0
---
+++ src/main.c
@@ -0,0 +1,33 @@
+/********************************** (C) COPYRIGHT *******************************
+* File Name          : main.c
+* Author             : WCH
+* Version            : V1.0.0
+* Date               : 2021/06/06
+* Description        : Main program body.
+*******************************************************************************/
+#include "debug.h"
+
+/*******************************************************************************
+* Function Name  : main
+* Description    : Main program.
+* Input          : None
+* Return         : None
+*******************************************************************************/
+int main(void)
+{
+	Delay_Init();
+	USART_Printf_Init(115200);
+
+	while(1){
+		Delay_Ms(100);
+		printf("SystemClk:%d\r\n",SystemCoreClock);
+		printf("WCH ^V^\r\n");
+	}
+}
+
+
+
+
+
+
+
