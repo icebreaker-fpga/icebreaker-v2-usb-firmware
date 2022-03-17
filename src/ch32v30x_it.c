561101fc5a6fedf63e7bfd1ebbecf646fad0910b
Ahmed Charles <acharles@outlook.com>
2022-03-17 06:48:42 -0700

    Fix interrupt handlers.

--- src/ch32v30x_it.c
+++ src/ch32v30x_it.c
@@ -1,40 +1,25 @@
 /********************************** (C) COPYRIGHT *******************************
-* File Name          : ch32v30x_it.c
-* Author             : WCH
-* Version            : V1.0.0
-* Date               : 2021/06/06
-* Description        : Main Interrupt Service Routines.
-* Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
-* SPDX-License-Identifier: Apache-2.0
-*******************************************************************************/
+ * File Name          : ch32v30x_it.c
+ * Author             : WCH
+ * Version            : V1.0.0
+ * Date               : 2021/06/06
+ * Description        : Main Interrupt Service Routines.
+ * Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
+ * Copyright (c) 2022 Ahmed Charles
+ * SPDX-License-Identifier: Apache-2.0
+ *******************************************************************************/
+
 #include "ch32v30x_it.h"
 
-void NMI_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
-void HardFault_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
+void NMI_Handler() __attribute__((naked));
+void HardFault_Handler() __attribute__((naked));
 
-/*********************************************************************
- * @fn      NMI_Handler
- *
- * @brief   This function handles NMI exception.
- *
- * @return  none
- */
-void NMI_Handler(void)
-{
+// This function handles NMI exception.
+void NMI_Handler() {
+  __asm volatile("mret");
 }
 
-/*********************************************************************
- * @fn      HardFault_Handler
- *
- * @brief   This function handles Hard Fault exception.
- *
- * @return  none
- */
-void HardFault_Handler(void)
-{
-  while (1)
-  {
-  }
+// This function handles Hard Fault exception.
+void HardFault_Handler() {
+  __asm volatile("1: j 1b");
 }
-
-
--- src/ch32v30x_it.h
+++ src/ch32v30x_it.h
@@ -1,18 +1,17 @@
 /********************************** (C) COPYRIGHT *******************************
-* File Name          : ch32v30x_it.h
-* Author             : WCH
-* Version            : V1.0.0
-* Date               : 2021/06/06
-* Description        : This file contains the headers of the interrupt handlers.
-* Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
-* SPDX-License-Identifier: Apache-2.0
-*******************************************************************************/
-#ifndef __CH32V30x_IT_H
-#define __CH32V30x_IT_H
+ * File Name          : ch32v30x_it.h
+ * Author             : WCH
+ * Version            : V1.0.0
+ * Date               : 2021/06/06
+ * Description        : This file contains the headers of the interrupt handlers.
+ * Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
+ * Copyright (c) 2022 Ahmed Charles
+ * SPDX-License-Identifier: Apache-2.0
+ *******************************************************************************/
+
+#ifndef CH32V30x_IT_H
+#define CH32V30x_IT_H
 
 #include "debug.h"
 
-
-#endif /* __CH32V30x_IT_H */
-
-
+#endif /* CH32V30x_IT_H */
